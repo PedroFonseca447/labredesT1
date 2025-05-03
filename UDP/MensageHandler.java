79d955b1e0af23107791202ad305bc7b591c7cf0
Mauricio <maugaspary@gmail.com>
2025-05-03 17:45:52 -0300

    Corrigi o ack não sendo enviado corretamente

--- UDP/MensageHandler.java
+++ UDP/MensageHandler.java
@@ -11,7 +11,7 @@ public class MensageHandler { // quem vai processar as mensagens recebidas via u
             else if( msg.startsWith("TALK")){
                 String[] talkMessage = msg.split(" ",3);
                 System.out.println("Recebido " + talkMessage[2]);//o conteudo da msg
-                String ack = "ACK" + talkMessage[1];
+                String ack = "ACK " + talkMessage[1];
                 server.sendMessage(ack,address,port);//registra o ack de quem enviou
             } else if(msg.startsWith("ACK")){
                 String idMsg = msg.split(" ", 2)[1];
